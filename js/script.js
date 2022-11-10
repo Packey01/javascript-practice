016acc1fe4373b4b7393307fb97c53c37c0e02ba
Andrea Packey <andreapackey@gmail.com>
2022-11-09 20:58:49 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-document.querySelector('button').innerHTML = 'Hello';
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
+let accountType = prompt('what are your first number?');
+console.log (parseInt(accountType));
+let accountType1 = prompt('what are your first number?');
+console.log (parseInt(accountType1));
+
+if (accountType === (accountType > accountType1))
+alert ('greater than');
+else (accountType === (accountType < accountType1))
+alert ('lesser than');
