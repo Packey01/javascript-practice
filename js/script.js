5c36f8ba700550174de89040f889179acb5e5fa1
Andrea Packey <andreapackey@gmail.com>
2022-11-14 21:06:13 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-// ------------ DAILY CHALLENGE ------------
-// Prompt the user for two different numbers
-// Convert the values to integers
-// Store the two integers as variables
-// Compare the two numbers
-// Alert the larger number
-let accountType = prompt('what are your first number?');
-console.log (parseInt(accountType));
-let accountType1 = prompt('what are your second number?');
-console.log (parseInt(accountType1));
 
-if (isNaN(accountType)) {
-    alert ("not a number, enter a number")
-  }
-  if (isNaN(accountType1)) {
-   alert ("not a number, enter a number")
-  }
-if (accountType === (accountType > accountType1))
-alert ('greater than');
-else (accountType === (accountType < accountType1))
-alert ('lesser than');
 
+myFunction = function x7(a) {
+    return a * 7;
+}
+
+
+
+
+const myObject {
+    myNumber: 9,
+
+    myMethod(a) {
+        return  a * this.myNumber;
+    }
+}
