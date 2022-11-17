e3906486aa7c4525403f07f055249c0b9732d96f
Andrea Packey <andreapackey@gmail.com>
2022-11-16 18:54:50 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
+//Validate the password using the following rules
 
+//Must be 6-20 characters long
+//Must start with a letter
 
-myFunction = function x7(a) {
-    return a * 7;
-}
-
-
-
-
-const myObject {
-    myNumber: 9,
-
-    myMethod(a) {
-        return  a * this.myNumber;
+function validate (input){
+    input = prompt('Please choose a password between 6 and 20 characters.  The first character must be a letter.')
+    let regex = /^[A-Za-z]\w{5,19}$/
+    let result = regex.test (input)
+    console.log(result)
+    if (result === true) {
+      alert('Your password is acceptable')
     }
-}
+    else {
+      alert('Your password is unnacceptable')
+      validate()
+    }
+  }
+  validate()
