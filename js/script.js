9eb3bde5bead1755e46e696272d22b46980def93
Andrea Packey <andreapackey@gmail.com>
2022-11-16 18:58:17 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 function validate (input){
     input = prompt('Please choose a password between 6 and 20 characters.  The first character must be a letter.')
     let regex = /^[A-Za-z]\w{5,19}$/
-    let result = regex.test (input)
+    result = regex.test (input)
     console.log(result)
     if (result === true) {
       alert('Your password is acceptable')
